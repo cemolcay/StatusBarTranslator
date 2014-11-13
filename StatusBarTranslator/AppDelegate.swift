6ea796a1f08a50216ded65ffd6c0fcc513cd59d3
Cem Olcay <cem@IT-91.local>
2014-11-13 10:59:31 +0200

    Initial Commit

---
+++ StatusBarTranslator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  StatusBarTranslator
+//
+//  Created by Cem Olcay on 13/11/14.
+//  Copyright (c) 2014 Cem Olcay. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
